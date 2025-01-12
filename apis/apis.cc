246550bcc94297563dda8a920829b3dc6dbaea70
keshuigu <keshuigu_yl@qq.com>
2025-01-12 19:48:44 +0800

    use epoll poll

--- apis/apis.cc
+++ apis/apis.cc
@@ -1,11 +1,13 @@
-#include <arpa/inet.h>   // inet_pton, inet_ntop 等函数
+#include <arpa/inet.h>  // inet_pton, inet_ntop 等函数
+#include <fcntl.h>
 #include <netdb.h>       // gethostbyname
 #include <netinet/in.h>  // sockaddr_in 结构体
 #include <signal.h>      // 中断
 #include <sys/socket.h>  // socket 函数及数据结构
-#include <sys/types.h>   // 数据类型定义
-#include <sys/un.h>      // struct sockeraddr_un
-#include <unistd.h>      // close 函数
+#include <sys/stat.h>
+#include <sys/types.h>  // 数据类型定义
+#include <sys/un.h>     // struct sockeraddr_un
+#include <unistd.h>     // close 函数
 
 #include <cassert>
 #include <cstring>   // memset, memcpy 等函数
@@ -155,17 +157,108 @@ void options() {
 void hostname() {
   struct hostent* res = gethostbyname("www.baidu.com");
   std::cout << res->h_name << res->h_addrtype << res->h_length;
-  struct servent* ress = getservbyname("daytime",NULL);
+  struct servent* ress = getservbyname("daytime", NULL);
   std::cout << ress->s_name;
+}
+
+void pipes() {
+  int fd[2];
+  // fd[1] = open("read.txt",O_CREAT|O_RDONLY);
+  // fd[2] = open("write.txt",O_CREAT|O_WRONLY);
+  pipe(fd);
+  char wbuf[100];
+  const char* s = "hello";
+  strncpy(wbuf, s, strlen(s));
+  write(fd[1], wbuf, strlen(s));
+  char rbuf[100];
+  read(fd[0], rbuf, strlen(s));
+  std::cout << rbuf;
+  close(fd[0]);
+  close(fd[1]);
+}
+
+void simpleCGI() {
+  const char* ip = "127.0.0.1";
+  int port = 12345;
+
+  struct sockaddr_in address;
+  bzero(&address, sizeof(address));
+  address.sin_family = AF_INET;
+  inet_pton(AF_INET, ip, &address.sin_addr);
+  address.sin_port = htons(port);
+
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+  assert(sock > 0);
+  int err = bind(sock, (struct sockaddr*)&address, sizeof(address));
+  assert(err != -1);
+  err = listen(sock, 5);
+  assert(err != -1);
+
+  // fd 是进程级资源
+  struct sockaddr_in client;
+  socklen_t client_length = sizeof(client);
+  int connfd = accept(sock, (struct sockaddr*)&client, &client_length);
+  assert(connfd > 0);
+  close(STDOUT_FILENO);
+  dup(connfd);
+  std::cout << "testcgi" << '\n';
+  close(connfd);
+  close(sock);
+}
+
+void simpleEcho() {
+  const char* ip = "127.0.0.1";
+  int port = 12345;
+
+  struct sockaddr_in address;
+  bzero(&address, sizeof(address));
+  address.sin_family = AF_INET;
+  inet_pton(AF_INET, ip, &address.sin_addr);
+  address.sin_port = htons(port);
+
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+  assert(sock > 0);
+  int err = bind(sock, (struct sockaddr*)&address, sizeof(address));
+  assert(err != -1);
+  err = listen(sock, 5);
+  assert(err != -1);
+
+  struct sockaddr_in client;
+  socklen_t client_length = sizeof(client);
+  int connfd = accept(sock, (struct sockaddr*)&client, &client_length);
+  assert(connfd > 0);
+  int pipefd[2];
+  err = pipe(pipefd);
+  assert(err != -1);
+  // 未实现监听链接断开
+  while (true) {
+    err = splice(connfd, NULL, pipefd[1], NULL, 32768,
+                 SPLICE_F_MORE | SPLICE_F_MOVE);
+    assert(err != -1);
+    err = splice(pipefd[0], NULL, connfd, NULL, 32768,
+                 SPLICE_F_MORE | SPLICE_F_MOVE);
+  }
+  close(connfd);
+  close(sock);
+}
+
+void testUid() {
+  uid_t uid = getuid();
+  uid_t euid = geteuid();
+  std::cout << uid << ' ' << euid << '\n';
 }
 
 }  // namespace apis
 
 int main() {
-  apis::bytetrans();
-  apis::printaddr();
+  // apis::bytetrans();
+  // apis::printaddr();
   // apis::sockets();
-  apis::options();
-  apis::hostname();
+  // apis::options();
+  // apis::hostname();
+  // apis::pipes();
+  // apis::simpleCGI();
+  // apis::simpleEcho();
+  apis::testUid();
   return 0;
 }
